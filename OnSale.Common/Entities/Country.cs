be52917d47ecce060469007dab4e6faa30d6a92b
jpinell <jpinell1075@gmail.com>
2020-11-11 17:21:36 -0600

    Hasta el appsetting.json

---
+++ OnSale.Common/Entities/Country.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace OnSale.Common.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        
+        [MaxLength(50)]
+        [Required]
+        public string Name { get; set; }
+    }
+}
