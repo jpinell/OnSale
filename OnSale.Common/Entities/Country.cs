9e5ecfb72ab27a161ebaa244ef842a92a3a284a9
jpinell <jpinell1075@gmail.com>
2020-11-13 14:55:18 -0600

    Cambios en las vistas del CRUD Country

--- OnSale.Common/Entities/Country.cs
+++ OnSale.Common/Entities/Country.cs
@@ -9,7 +9,7 @@ namespace OnSale.Common.Entities
     {
         public int Id { get; set; }
         
-        [MaxLength(50)]
+        [MaxLength(50, ErrorMessage = "El campo {0} Debe contener hasta {1} caracteres")]
         [Required]
         public string Name { get; set; }
     }
